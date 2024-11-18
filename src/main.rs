4fb34aece7105925052f7decbdbe1121c96d7a33
faliah <leogoldf21@gmail.com>
2024-11-18 12:07:07 +0100

    support implemented in runtime

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod balances;
 mod support;
 mod system;
+
+use crate::support::Dispatch;
 mod types {
     use crate::support;
 
@@ -23,14 +25,31 @@ pub enum RuntimeCall {
 }
 
 impl system::Config for Runtime {
-    type AccountId = String;
-    type BlockNumber = u32;
-    type Nonce = u32;
+    type AccountId = types::AccountId;
+    type BlockNumber = types::BlockNumber;
+    type Nonce = types::Nonce;
 }
 
 impl balances::Config for Runtime {
-    type Balance = u32;
+    type Balance = types::Balance;
 }
+impl crate::support::Dispatch for Runtime {
+    type Caller = <Runtime as system::Config>::AccountId;
+    type Call = RuntimeCall;
+    // Dispatch a call on behalf of a caller. Increments the caller's nonce.
+    //
+    // Dispatch allows us to identify which underlying module call we want to execute.
+    // Note that we extract the `caller` from the extrinsic, and use that information
+    // to determine who we are executing the call on behalf of.
+    fn dispatch(
+        &mut self,
+        caller: Self::Caller,
+        runtime_call: Self::Call,
+    ) -> support::DispatchResult {
+        unimplemented!();
+    }
+}
+
 // This is our main Runtime.
 // It accumulates all of the different pallets we want to use.
 #[derive(Debug)]
@@ -50,6 +69,27 @@ impl Runtime {
             system: system::Pallet::new(),
             balances: balances::Pallet::new(),
         }
+    }
+
+    // Execute a block of extrinsics. Increments the block number.
+    fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+        self.system.inc_block_number();
+        if self.system.block_number() != block.header.block_number {
+            return Err("Block number mismatched");
+        }
+        for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
+            self.system.inc_nonce(&caller);
+
+            let _ = self.dispatch(caller, call).map_err(|e| {
+                eprintln!(
+                    "Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+                    block.header.block_number, i, e
+                )
+            });
+            // do stuff with `caller` and `call`
+        }
+
+        Ok(())
     }
 }
 
