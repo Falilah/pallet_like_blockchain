5318d6022d2774583b5def0a3ecfa77e38e8b527
faliah <leogoldf21@gmail.com>
2024-11-20 16:49:56 +0100

    call dispatched through block

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@ mod types {
 // These are all the calls which are exposed to the world.
 // Note that it is just an accumulation of the calls exposed by each module.
 pub enum RuntimeCall {
-    // TODO: Not implemented yet.
+    BalancesTransfer { to: types::AccountId, amount: types::Balance },
+
 }
 
 impl system::Config for Runtime {
@@ -46,7 +47,12 @@ impl crate::support::Dispatch for Runtime {
         caller: Self::Caller,
         runtime_call: Self::Call,
     ) -> support::DispatchResult {
-        unimplemented!();
+        match runtime_call {
+            RuntimeCall::BalancesTransfer { to, amount } => {
+                self.balances.transfer(&caller, &to, amount)?;
+            }
+        }
+        Ok(())
     }
 }
 
@@ -96,42 +102,68 @@ impl Runtime {
 fn main() {
     /* Create a mutable variable `runtime`, which is a new instance of `Runtime`. */
     let mut runtime = Runtime::new();
-    let alice = &"Alice".to_string();
-    let bob = &"Bob".to_string();
+    let alice = "Alice".to_string();
+    let bob = "Bob".to_string();
     let charlie = &"Charlie".to_string();
 
     /* Set the balance of `alice` to 100, allowing us to execute other transactions. */
-    runtime.balances.set_balance(alice, 100);
+    runtime.balances.set_balance(&alice, 100);
+    let block_1 = types::Block {
+        header: support::Header { block_number: 1 },
+        extrinsics: vec![
+            support::Extrinsic {
+                caller: alice,
+                call: RuntimeCall::BalancesTransfer { to: bob, amount: 69 },
+            },
+            support::Extrinsic {
+                caller: "Bob".to_string(),
+                call: RuntimeCall::BalancesTransfer { to: "Charlie".to_string(), amount: 30 },
+            },
+            support::Extrinsic {
+                caller: "alex".to_string(),
+                call: RuntimeCall::BalancesTransfer { to: "Charlie".to_string(), amount: 30 },
+            },
+            support::Extrinsic {
+                caller: "Alice".to_string(),
+                call: RuntimeCall::BalancesTransfer { to: "alex".to_string(), amount: 10 },
+            },
+        ],
+    };
+
+    runtime.execute_block(block_1).expect("invalid block");
+
 
     // start emulating a block
     /* Increment the block number in system. */
-    runtime.system.inc_block_number();
-    /* Assert the block number is what we expect. */
-    assert!(runtime.system.block_number() == 1);
+    // runtime.system.inc_block_number();
+    // /* Assert the block number is what we expect. */
+    // assert!(runtime.system.block_number() == 1);
 
     // first transaction
-    /* Increment the nonce of `alice`. */
-    runtime.system.inc_nonce(alice);
+    // /* Increment the nonce of `alice`. */
+    // runtime.system.inc_nonce(alice);
 
-    /*Execute a transfer from `alice` to `bob` for 30 tokens.
-        - The transfer _could_ return an error. We should use `map_err` to print
-          the error if there is one.
-        - We should capture the result of the transfer in an unused variable like `_res`.
-    */
+    // /*Execute a transfer from `alice` to `bob` for 30 tokens.
+    //     - The transfer _could_ return an error. We should use `map_err` to print
+    //       the error if there is one.
+    //     - We should capture the result of the transfer in an unused variable like `_res`.
+    // */
 
-    let _res = runtime
-        .balances
-        .transfer(alice, bob, 30)
-        .map_err(|e| eprintln!("{}", e));
+    // let _res = runtime
+    //     .balances
+    //     .transfer(alice, bob, 30)
+    //     .map_err(|e| eprintln!("{}", e));
 
-    // second transaction
-    /* Increment the nonce of `alice` again. */
-    runtime.system.inc_nonce(alice);
-    /* Execute another balance transfer, this time from `alice` to `charlie` for 20. */
-    let _res = runtime
-        .balances
-        .transfer(alice, charlie, 20)
-        .map_err(|e| eprintln!("{}", e));
+    // // second transaction
+    // /* Increment the nonce of `alice` again. */
+    // runtime.system.inc_nonce(alice);
+    // /* Execute another balance transfer, this time from `alice` to `charlie` for 20. */
+    // let _res = runtime
+    //     .balances
+    //     .transfer(alice, charlie, 20)
+        // .map_err(|e| eprintln!("{}", e));
+
+        
 
     println!("{:?}", runtime)
 }
